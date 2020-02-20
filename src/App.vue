0791d1015adab62ca3a0b71e53039cf8c5fbc021
akshaykn <akshaykrishna633@gmail.com>
2020-02-20 21:47:16 +0530

    fourth  build

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <div>   </di
+    <div>   </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
